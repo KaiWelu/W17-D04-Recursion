9b3cfddf76a6832f88844080d82495c3428fdd65
KaiWelu <kaiweluda@gmail.com>
2025-02-06 17:21:37 +0100

    adds problem 4

--- labs/recursion/src/main/java/org/dci/Main.java
+++ labs/recursion/src/main/java/org/dci/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println(countDigits(123456789));
         System.out.println(isPalindrome("AnNa"));
         System.out.println(sumOfDigits(-55555111));
+        int[] testArray = {1, 2, 3, 4, 5};
+        printReverse(testArray, testArray.length - 1);
     }
 
     public static int countDigits(int n) {
@@ -40,5 +42,10 @@ public class Main {
         if(n == 0) return 0;
 
         return n % 10 + sumOfDigits(n / 10);
+    }
+
+    public static void printReverse(int[] arr, int index) {
+        System.out.println(arr[index]);
+        if(index > 0) printReverse(arr, index -1);
     }
 }
