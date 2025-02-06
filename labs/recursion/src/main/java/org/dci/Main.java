d09c413993911653e299c5ff111e3c178c052955
KaiWelu <kaiweluda@gmail.com>
2025-02-06 17:09:19 +0100

    adds problem 2

--- labs/recursion/src/main/java/org/dci/Main.java
+++ labs/recursion/src/main/java/org/dci/Main.java
@@ -1,8 +1,11 @@
 package org.dci;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(countDigits(123456789));
+        System.out.println(isPalindrome("AnNa"));
     }
 
     public static int countDigits(int n) {
@@ -13,5 +16,19 @@ public class Main {
         }
         return 1 + countDigits(n / 10);
 
+    }
+
+    public static boolean isPalindrome(String s) {
+        s =  s.toLowerCase();
+        
+        if(s.length() <= 1) {
+            return true;
+        }
+
+        if(s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+
+        return isPalindrome(s.substring(1, s.length()- 1));
     }
 }
